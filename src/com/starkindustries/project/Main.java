80c266aed687d10cb2b97412ec867200a9a54ced
liu zi an <liuzian@stark.com>
2020-04-20 12:15:10 +0800

    added database to the program

---
+++ src/com/starkindustries/project/Main.java
@@ -0,0 +1,37 @@
+package com.starkindustries.project;
+
+import java.net.URISyntaxException;
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.List;
+
+
+public class Main {
+
+	public static void main(String[] args) throws URISyntaxException, SQLException {
+		
+		/*
+		 * 1. login to useradmin
+		 * - check username and password
+		 * 
+		 * 
+		 * 2. register/create user account (done)
+		 * - check if username taken
+		 * 
+		 * 
+		 * 3. suspend or unsuspend user
+		 * - get user suspend status by username
+		 * - update isSuspended to true or false
+		 * 
+		 * 
+		 * 4. reset username password
+		 * - get user info by username
+		 * - update password
+		 *
+		 *
+		 * 5. view all student and moderator info (done)
+		 * - get all user info
+		 * */ 
+	}
+}
