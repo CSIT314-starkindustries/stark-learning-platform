2c22fc60b000bb4c70eb1c760c38d83e56ba4490
Sheng Yan <63837257+btecsolid@users.noreply.github.com>
2020-04-21 15:55:44 +0800

    Basic User Validation System

--- src/com/starkindustries/project/Main.java
+++ src/com/starkindustries/project/Main.java
@@ -2,8 +2,11 @@ package com.starkindustries.project;
 
 import java.net.URISyntaxException;
 import java.sql.Connection;
+import java.sql.Statement;
+import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.Scanner;
 import java.util.List;
 
 
@@ -11,6 +14,40 @@ public class Main {
 
 	public static void main(String[] args) throws URISyntaxException, SQLException {
 		
+		System.out.println("Login as (Number):");
+		System.out.println("1: Student");
+		System.out.println("2: Moderator");
+		System.out.println("3: User Admin");
+		System.out.print("Enter Number: ");
+		Scanner input = new Scanner(System.in);
+		int num = input.nextInt();
+		
+		System.out.print("Username: ");
+		String user = input.next();
+		
+		System.out.print("Password: ");
+		String pass = input.next();
+		
+		boolean success = new Validation().validateUser(num,user,pass);
+		
+		if (success) {
+			System.out.println("User Validation successful.");
+		}
+		else {
+			System.out.println("User Validation failed.");
+		}
+		
+		/*
+		Connection conn = new StarkDatabase().getConn();
+		Statement stmt = conn.createStatement();
+		ResultSet rs = stmt.executeQuery("select * from public.user_admin");
+		while (rs.next()) {
+			String username = rs.getString("ua_username");
+			String password = rs.getString("ua_password");
+			System.out.println(username + " " + password);
+		}
+		conn.close();
+		*/
 		/*
 		 * 1. login to useradmin
 		 * - check username and password
--- src/com/starkindustries/project/StarkDatabase.java
+++ src/com/starkindustries/project/StarkDatabase.java
@@ -10,7 +10,7 @@ public class StarkDatabase {
 	}
 	public Connection getConn() throws URISyntaxException, SQLException {
 		URI dbUri = new URI(System.getenv("DATABASE_URL"));
-		 
+		
 		String username = dbUri.getUserInfo().split(":")[0];
 		String password = dbUri.getUserInfo().split(":")[1];
 		String dbUrl = "jdbc:postgresql://" + dbUri.getHost() + ':' + dbUri.getPort() + dbUri.getPath();
