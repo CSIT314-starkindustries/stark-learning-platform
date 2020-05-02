cfc42975689620978f067ec1665e5e0d0b5987d0
beatricia <beatricia.inspirit@gmail.com>
2020-05-02 17:28:46 +0800

    Commit

--- WebContent/js/studentHome.js
+++ WebContent/js/studentHome.js
@@ -3,3 +3,12 @@ function scrollToTopFunction() {
 	document.body.scrollTop = 0;
 	document.documentElement.scrollTop = 0;
 }
+
+/*
+function search() {
+	document.getElementsById('searchForm').submit(); // Form submission
+	
+	var y = document.getElementsById('searchResultPane');
+	document.style.display = y;
+} 
+*/
